ee0d7012c1a473080d983d97a6968db175f1e16f
Sans2k <140954881+Sans2k@users.noreply.github.com>
2023-10-17 08:13:37 +0700

    Add files via upload

---
+++ windowsObject.js
@@ -0,0 +1,9 @@
+let myWindow;
+
+function openWin() {
+  myWindow = window.open("https://www.instagram.com/sans_e4k", "_blank", "width=500, height=500");
+}
+
+function closeWin() {
+  myWindow.close();
+}
